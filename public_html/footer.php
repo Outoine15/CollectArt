2c3266d25380f62e558775d7ec82060d0b2b0c69
LeReblochon <louis.prinaire@gmail.com>
2025-04-14 18:08:39 +0200

    Add files via upload
    J'ai changé index, header, footer pour que tout fonctionne pk il y avait deux trois erreurs.

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,22 +1,53 @@
 <?php
-//$target="";
-//$link="image/Logo_USMB_web_vertical_grand_RVB.png";
 echo "
-<style>
+
+
+
+	<style>
+	body{
+		margin:0;
+		padding:0;}
+	
 footer {
-  display: flex;
-  justify-content: center;
+	margin:0px;
+	padding:0px;
+    background-color: #537FE7;
+    color: rgba(188, 207, 214, 0.925);
+    position:fixed;
+    bottom: 0;
+	width : 100%;
+	height: 10%;
+	padding-bottom:2%;
+	padding-top:1%;}
 
-  padding: 5px;
-  background-color: #45a1ff;
-  color: #fff;
-
+#div-image-univsmb{
+position: absolute;
+	top:0;
+	right:0;
+}	
+	
+#image-univsmb{
+	height:9%;	
+	
 }
 
-</style>
-  <footer>
-    <p>orgen</p>
-	<image src=\"image/Logo_USMB_web_vertical_grand_RVB.png\"></image>
-  </footer>
+#div_droits{
+	display:flex;
+	flex-direction:column;
+	justify-content: center;
+	text-align:center;}
+	 </style>
+	 
+<footer>
+      
+	  <div id=\"div_droits\">  
+	  <p>@ Tout droit r&#233;serv&#233; &#224; Bob_et_Alice</p>
+	  <p>Les cookies c'est pas mauvais</p>
+	  </div>
+
+      <a href=\"https://www.univ-smb.fr/\" id=\"div-image-univsmb\"><image id=\"image-univsmb\" src=\"images/Logo_USMB_web_vertical_grand_RVB.png\"></image></a>
+
+
+</footer>
 ";
 ?>
