64256ffae4a5ea9efec7542ca7ed9677dcc28efb
outoine15 <outoine15@gmail.com>
2025-04-25 11:37:45 +0200

    un todo de plus

--- public_html/dom/dom_main.js
+++ public_html/dom/dom_main.js
@@ -8,13 +8,15 @@ function affiche_list_toiles(){
         let div = document.createElement("div");
         let link = document.createElement("a");
         const toile = list_toiles[index];
+        // valeurs temporaires (TODO:supprimer quand elles serons plus undefined (quand elles serons dans la db))
         toile["hauteur"]=30;
         toile["largeur"]=30;
         link.innerHTML+=toile["name"];
         link.href="toile_edit/toile_edit.php?id="+toile["id"]+"&name="+toile["name"]+"&hauteur="+toile["hauteur"]+"&largeur="+toile["largeur"];
         div.id=toile["id"];
-        div.innerHTML+=toile["description"];
         div.appendChild(link);
+        div.innerHTML+=" description: "
+        div.innerHTML+=toile["description"];
         main_div.appendChild(div);
     }
     console.log(main_div);
