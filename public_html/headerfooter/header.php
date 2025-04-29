cf623f381f7cc29dc38d74b15f64dbc030bc45be
outoine15 <outoine15@gmail.com>
2025-04-29 16:07:44 +0200

    le header c'est bon

--- public_html/headerfooter/header.php
+++ public_html/headerfooter/header.php
@@ -3,17 +3,21 @@ echo '
 
 <header>
 <nav>
+
 <div class="nav-title">
-    <h1>CollectArt</h1>
+    <a href="../index.php"><h1>CollectArt</h1></a>
 </div>
+
 <div class="nav-header-buttons">
 <a href="../autre.php">autre</a>
 <a href="~/toile_edit/toile_edit.php">editer la toile</a>
 <a href="~/.php">autre</a>
 </div>
-<div id="connection">
-<a href="../main/index.php">Connexion au compte</a>
-</div>
+
+<div class="connection">
+<a href="../user/connUser.php">Connexion au compte</a>
+</div> 
+
 </nav>
 </header>
 ';
