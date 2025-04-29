963bbe17b5374f258c46c6290ff4b2613e3afcc4
tetedepian0 <nathan.martin.odienne@gmail.com>
2025-04-29 17:48:12 +0200

    r'ajout de tous les css qui sont sur le serv( ou commit des modifs qui etaient que sur le serv)

--- public_html/headerfooter/header.php
+++ public_html/headerfooter/header.php
@@ -3,17 +3,21 @@ echo '
 
 <header>
 <nav>
+
 <div class="nav-title">
     <h1>CollectArt</h1>
 </div>
+
 <div class="nav-header-buttons">
 <a href="../autre.php">autre</a>
 <a href="~/toile_edit/toile_edit.php">editer la toile</a>
 <a href="~/.php">autre</a>
 </div>
+
 <div id="connection">
 <a href="../main/index.php">Connexion au compte</a>
 </div>
+
 </nav>
 </header>
 ';
