bb89111ae7421714b4fa6ce5b2e18e4b626c7a41
tetedepian0 <nathan.martin.odienne@gmail.com>
2025-04-29 18:13:10 +0200

    changement chelou anciennes versions.............

--- public_html/user/connUser.php
+++ public_html/user/connUser.php
@@ -20,17 +20,43 @@ include("../DBconnect/db_disconnect.php");
 ?>
 
 
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-<html>
-	<head>
-	<meta charset="UTF-8">
-	</head>
-		<link rel="stylesheet" href="connUser.css">
-	<body>
-	<?php
-	include("../header.php");?>
-	
-	<div id="formulaire">
+
+include("../crud/user.crud.php");
+include("../DBconnect/db_connect.php");
+
+
+if(isset($_POST["login"])){
+	$estdansBDD = is_in_DB($_POST["login"] , $_POST["passwd"] , $conn);
+	if($estdansBDD=='1'){
+		header("Location: rien.php");
+	}
+}
+
+
+
+include("../DBconnect/db_disconnect.php");
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/default.css">
+    <link rel="stylesheet" href="../css/form.css">
+    <title>Se connecter</title>
+    <script src="../script/page404.js"></script>
+</head>
+<body>
+<?php
+include("../headerfooter/header.php");
+?>
+
+<div id="container">
+    	<div id="formulaire">
 <form method="POST" action="connUser.php">
 	<p>Nom d'utilisateur</p>
 	<input type="text" name="login">
@@ -41,25 +67,10 @@ include("../DBconnect/db_disconnect.php");
 	</form>
 	<a href="creeUser.php">Créer un compte</a>
 	</div>
-	<?php
-	include("../footer.php");?>
-	</body>
-	 
-<footer>
-      
-	  <div id="div_droits">  
-	  <p>@ Tout droit r&#233;serv&#233; &#224; Bob_et_Alice</p>
-	  <p>Les cookies c'est pas mauvais</p>
-	  </div>
+</div> 
 
-      <a href="https://www.univ-smb.fr/" id="div-image-univsmb"><image id="image-univsmb" src="../images/Logo_USMB_web_vertical_grand_RVB.png"></image></a>
-
-
-</footer>
-
-	</body>
+<?php
+include("../headerfooter/footer.php");
+?>    
+</body>
 </html>
-</html>
-
-
-
