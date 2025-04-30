4759b4f76314b7968cef3eeca86fb9518144ebb3
outoine15 <outoine15@gmail.com>
2025-04-30 21:39:16 +0200

    joli toile_cree

--- public_html/toile_cree/cree_toile.php
+++ public_html/toile_cree/cree_toile.php
@@ -1,41 +1,48 @@
 <?php
 session_start();
-if(!isset($_SESSION["user"])){
+if (!isset($_SESSION["user"])) {
     header("Location: ../user/connUser.php");
 }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/default.css">
     <title>créer une toile</title>
 </head>
-<body>
-<?php
-include("../headerfooter/header.php");
-?>
 
-<form action="toile.php" method="post">
+<body>
+    <?php
+    include("../headerfooter/header.php");
+    ?>
+
+    <form action="toile.php" method="post">
         <table>
             <tr>
-                <td>Nom</td><td><input type="text" name="nom"></td>
+                <td>Nom</td>
+                <td><input type="text" name="nom"></td>
             </tr>
             <tr>
-                <td>hauteur</td><td><input type="text" name="hauteur"></td>
+                <td>hauteur</td>
+                <td><input type="text" name="hauteur"></td>
             </tr>
             <tr>
-                <td>largeur</td><td><input type="text" name="largeur"></td>
+                <td>largeur</td>
+                <td><input type="text" name="largeur"></td>
             </tr>
             <tr>
-                <td>description</td><td><input type="text" name="description"></td>
+                <td>description</td>
+                <td><input type="text" name="description"></td>
             </tr>
         </table>
         <input type="submit" value="créer">
     </form>
-<?php
-include("../headerfooter/footer.php");
-?>
+    <?php
+    include("../headerfooter/footer.php");
+    ?>
 </body>
+
 </html>
--- public_html/toile_cree/toile.php
+++ public_html/toile_cree/toile.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
-if(!isset($_SESSION["user"])){
+if (!isset($_SESSION["user"])) {
     header("Location: ../user/connUser.php");
 }
 include("../headerfooter/header.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,34 +15,35 @@ include("../headerfooter/header.php");
     <link rel="stylesheet" href="../css/default.css">
     <title>toile en cours de création</title>
 </head>
+
 <body>
-<div id="container">
-<h1>votre toile est en cours de création</h1>
-<a href="../pages/mes_toiles.php">retour a mes toiles</a>
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-include("../DBconnect/db_connect.php");
-include("../crud/toile.crud.php");
-if(isset($_POST["nom"]) && isset($_POST["hauteur"]) && isset($_POST["largeur"]) && isset($_POST["description"])){
-    $nom=$_POST["nom"];
-    $description=$_POST["description"];
-    $hauteur=$_POST["hauteur"];
-    $largeur=$_POST["largeur"];
-    $id_creator=0;
-    if($hauteur > 100 || $largeur > 100){
-        echo "<div>toile trop grand, vous voullez tous casser ou quoi? (max 100px hauteur/largeur)</div>";
-    } else{
-        if(isset($_SESSION["user"])){
-            $id_creator=(int)$_SESSION["user"];
-        }
-        // print_r($id_creator);
-        insert_toile($conn,$nom,$description,$id_creator,$hauteur,$largeur,0);
-        $id=get_last_inserted_id($conn);
-        file_put_contents("../toilesJSON/$id.json","[]");
-        $toile_data=file_get_contents("../toilesJSON/$id.json");
-        echo "<script>\n";
-        echo "var toile_status = {
+    <div id="container">
+        <h1>votre toile est en cours de création</h1>
+        <a href="../pages/mes_toiles.php">retour a mes toiles</a>
+        <?php
+        error_reporting(E_ALL);
+        ini_set('display_errors', '1');
+        include("../DBconnect/db_connect.php");
+        include("../crud/toile.crud.php");
+        if (isset($_POST["nom"]) && isset($_POST["hauteur"]) && isset($_POST["largeur"]) && isset($_POST["description"])) {
+            $nom = $_POST["nom"];
+            $description = $_POST["description"];
+            $hauteur = $_POST["hauteur"];
+            $largeur = $_POST["largeur"];
+            $id_creator = 0;
+            if ($hauteur > 100 || $largeur > 100) {
+                echo "<div>toile trop grand, vous voullez tous casser ou quoi? (max 100px hauteur/largeur)</div>";
+            } else {
+                if (isset($_SESSION["user"])) {
+                    $id_creator = (int) $_SESSION["user"];
+                }
+                // print_r($id_creator);
+                insert_toile($conn, $nom, $description, $id_creator, $hauteur, $largeur, 0);
+                $id = get_last_inserted_id($conn);
+                file_put_contents("../toilesJSON/$id.json", "[]");
+                $toile_data = file_get_contents("../toilesJSON/$id.json");
+                echo "<script>\n";
+                echo "var toile_status = {
                 'nom': '$nom',
                 'toile_id': $id,
                 'hauteur': $hauteur,
@@ -56,14 +58,15 @@ if(isset($_POST["nom"]) && isset($_POST["hauteur"]) && isset($_POST["largeur"]) 
                 // echo "load_json_data();\n";
                 echo "console.log(toile_status);\n";
             }
-        echo "</script>";
-    }
-// header("Location: ../pages/mes_toiles.php");
-?>
-</div>
+            echo "</script>";
+        }
+        // header("Location: ../pages/mes_toiles.php");
+        ?>
+    </div>
 
-<?php
-include("../headerfooter/footer.php");
-?>
+    <?php
+    include("../headerfooter/footer.php");
+    ?>
 </body>
+
 </html>
