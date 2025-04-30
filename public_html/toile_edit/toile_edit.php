e3558a97495d23b5cdb78a944b60757c6288c209
outoine15 <outoine15@gmail.com>
2025-04-30 21:39:47 +0200

    joli toile_edit

--- public_html/toile_edit/toile_edit.php
+++ public_html/toile_edit/toile_edit.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,34 +9,35 @@
     <link rel="stylesheet" href="../css/default.css">
     <title>Document</title>
 </head>
-<body>
-<?php
-include("../headerfooter/header.php");
-?>
 
-<!-- <filesMatch ".php">
+<body>
+    <?php
+    include("../headerfooter/header.php");
+    ?>
+
+    <!-- <filesMatch ".php">
     Header set Cache-Control "max-age=84600, public"
 </filesMatch> -->
 
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+    <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
 
-include("../DBconnect/db_connect.php");
-include("../crud/toile.crud.php");
+    include("../DBconnect/db_connect.php");
+    include("../crud/toile.crud.php");
 
-$html = "<div id='container'>";
+    $html = "<div id='container'>";
 
-// ça marche si ya beaucoup de data dans la db json on dirait donc poser pas trop de questions
-if(isset($_GET["id"]) && isset($_GET["name"]) && isset($_GET["hauteur"]) && isset($_GET["largeur"])){
-    $id=$_GET["id"];
-    $nom=$_GET["name"];
-    $hauteur=$_GET["hauteur"];
-    $largeur=$_GET["largeur"];
-    $toile_data=file_get_contents("../toilesJSON/$id.json");
-    echo "<h1>$nom</h1>\n";
-    echo "<script>\n";
-    echo "var toile_status = {
+    // ça marche si ya beaucoup de data dans la db json on dirait donc poser pas trop de questions
+    if (isset($_GET["id"]) && isset($_GET["name"]) && isset($_GET["hauteur"]) && isset($_GET["largeur"])) {
+        $id = $_GET["id"];
+        $nom = $_GET["name"];
+        $hauteur = $_GET["hauteur"];
+        $largeur = $_GET["largeur"];
+        $toile_data = file_get_contents("../toilesJSON/$id.json");
+        echo "<h1>$nom</h1>\n";
+        echo "<script>\n";
+        echo "var toile_status = {
             'nom': '$nom',
             'toile_id': $id,
             'hauteur': $hauteur,
@@ -45,21 +47,22 @@ if(isset($_GET["id"]) && isset($_GET["name"]) && isset($_GET["hauteur"]) && isse
             'loadData' : $toile_data,
             isDrawing : false
             };\n";
-            echo "make_toile();\n";
-            echo "load_json_data();\n";
-            echo "console.log(toile_status);\n";
-    echo "</script>\n";
-}
+        echo "make_toile();\n";
+        echo "load_json_data();\n";
+        echo "console.log(toile_status);\n";
+        echo "</script>\n";
+    }
 
-$html .= "</div>";
-echo $html;
+    $html .= "</div>";
+    echo $html;
 
-?>
+    ?>
 
-</div>
+    </div>
 
-<?php
-include("../headerfooter/footer.php");
-?>
+    <?php
+    include("../headerfooter/footer.php");
+    ?>
 </body>
+
 </html>
