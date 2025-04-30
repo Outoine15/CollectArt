767d97b73a6d16b9124f416cbd99437a66799367
outoine15 <outoine15@gmail.com>
2025-04-30 17:09:34 +0200

    le début de toile settings

---
+++ public_html/pages/toile_setting.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include("../DBconnect/db_connect.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>parramètres de toile</title>
+</head>
+
+<body>
+    <?php
+    include("../headerfooter/header.php");
+    ?>
+    <div id="container">
+<?php
+    if(isset($_GET["id"]));
+    echo "<h1>paramètres de la toile $toile_name</h1>"
+
+?>
+    </div>
+    <?php
+    include("../headerfooter/footer.php");
+    ?>
+</body>
+
+</html>
+<?php
+include("../DBconnect/db_disconnect.php");
+?>
