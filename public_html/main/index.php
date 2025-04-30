f0393f349e0f309d8de2f9adcc3ae909bc44d9e4
outoine15 <outoine15@gmail.com>
2025-04-30 21:37:55 +0200

    joli main

--- public_html/main/index.php
+++ public_html/main/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <script src="../dom/script.js"></script>
@@ -7,31 +8,33 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/default.css">
 </head>
+
 <body>
 
-<?php
-include("../DBconnect/db_connect.php");
-include("../headerfooter/header.php");
-include("../crud/toile.crud.php");
-?>
+    <?php
+    include("../DBconnect/db_connect.php");
+    include("../headerfooter/header.php");
+    include("../crud/toile.crud.php");
+    ?>
 
-<div id="container">
-<div id="toiles-container"></div>
-</div>
+    <div id="container">
+        <div id="toiles-container"></div>
+    </div>
 
-<?php
-$toiles=select_toiles($conn);
-$toiles=json_encode($toiles);
-// print_r($toiles);
-echo "<script>";
-echo "var listToiles = ${toiles};\n";
-echo "displayToiles()";
-echo "</script>";
+    <?php
+    $toiles = select_toiles($conn);
+    $toiles = json_encode($toiles);
+    // print_r($toiles);
+    echo "<script>";
+    echo "var listToiles = ${toiles};\n";
+    echo "displayToiles()";
+    echo "</script>";
 
-include("../headerfooter/footer.php");
+    include("../headerfooter/footer.php");
 
-include("../DBconnect/db_disconnect.php");
-?>
+    include("../DBconnect/db_disconnect.php");
+    ?>
 
 </body>
+
 </html>
