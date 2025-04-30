cc7b686bef35301e8a0f4fd8b1289245daf1532a
outoine15 <outoine15@gmail.com>
2025-04-30 21:41:03 +0200

    joli user

--- public_html/user/connUser.php
+++ public_html/user/connUser.php
@@ -4,29 +4,29 @@ include("../DBconnect/db_connect.php");
 
 session_start();
 
-if(isset($_SESSION["user"])){
+if (isset($_SESSION["user"])) {
 	header("Location: user.php");
 }
 
 $message_connexion = "";
 
-if(isset($_POST["user_name"]) && isset($_POST["user_pwd"])){
+if (isset($_POST["user_name"]) && isset($_POST["user_pwd"])) {
 	$name = $_POST["user_name"];
 	$pwd = $_POST["user_pwd"];
 
 	$user = get_user_account($conn, $name, $pwd);
 
-    if($user == []){
+	if ($user == []) {
 		$message_connexion .= "<p class='error_message'>Erreur : Identifiant et/ou mot de passe invalide</p>";
-	}else{
+	} else {
 		/* session user */
-		$_SESSION["user"] = $user["id"]; 
-		
+		$_SESSION["user"] = $user["id"];
+
 		/* redirection */
 		header("Location: ../pages/mes_toiles.php");
 	}
-}else if(isset($_GET["erreur_page"])){
-	if($_GET["erreur_page"] == "mes_toiles"){
+} else if (isset($_GET["erreur_page"])) {
+	if ($_GET["erreur_page"] == "mes_toiles") {
 		$message_connexion .= "<p class='error_message'>Erreur : Vous devez être connecté pour visualiser vos toiles</p>";
 	}
 }
@@ -35,69 +35,73 @@ if(isset($_POST["user_name"]) && isset($_POST["user_pwd"])){
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/default.css">
-    <link rel="stylesheet" href="../css/form.css">
-    <title>Se connecter</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../css/default.css">
+	<link rel="stylesheet" href="../css/form.css">
+	<title>Se connecter</title>
 	<script>
-	function passwordSeeHide(checkboxId, pwdId){
-		var checkbox = document.getElementById(checkboxId);
-		var pwd = document.getElementById(pwdId);
-		
-		if(checkbox.checked){
-			pwd.type = "text";
-		}else{
-			pwd.type = "password";
+		function passwordSeeHide(checkboxId, pwdId) {
+			var checkbox = document.getElementById(checkboxId);
+			var pwd = document.getElementById(pwdId);
+
+			if (checkbox.checked) {
+				pwd.type = "text";
+			} else {
+				pwd.type = "password";
+			}
 		}
-	}
 	</script>
 </head>
+
 <body>
-<?php
-include("../headerfooter/header.php");
-?>
+	<?php
+	include("../headerfooter/header.php");
+	?>
 
-<div id="container">
-	<div id="form_container">
-		<h2 class="form_title">Se connecter</h2>
-		<?php
-		echo $message_connexion;
-		?>
+	<div id="container">
+		<div id="form_container">
+			<h2 class="form_title">Se connecter</h2>
+			<?php
+			echo $message_connexion;
+			?>
 
-		<div id="formulaire">
-			<form method="POST" action="connUser.php">
-				<div class="form_section">
-					<p class="form_txt">Nom d'utilisateur</p>
-					<input type="text" name="user_name">
-				</div>
-
-				<div class="form_section">
-					<div class="form_header">
-						<p class="form_txt">Mot de passe</p>
-
-						<div class="pwd_container">
-							<input type="checkbox" id="checkbox_pwd" onclick="passwordSeeHide('checkbox_pwd', 'user_pwd')">
-							<label for="checkbox_pwd">Afficher</label>
-						</div>
+			<div id="formulaire">
+				<form method="POST" action="connUser.php">
+					<div class="form_section">
+						<p class="form_txt">Nom d'utilisateur</p>
+						<input type="text" name="user_name">
 					</div>
-						
-					<input type="password" name="user_pwd" id="user_pwd">
-				</div>
 
-				<input type="submit" value="Se connecter">
-			</form>
+					<div class="form_section">
+						<div class="form_header">
+							<p class="form_txt">Mot de passe</p>
 
-			<a href="creeUser.php">Créer un compte</a>
+							<div class="pwd_container">
+								<input type="checkbox" id="checkbox_pwd"
+									onclick="passwordSeeHide('checkbox_pwd', 'user_pwd')">
+								<label for="checkbox_pwd">Afficher</label>
+							</div>
+						</div>
+
+						<input type="password" name="user_pwd" id="user_pwd">
+					</div>
+
+					<input type="submit" value="Se connecter">
+				</form>
+
+				<a href="creeUser.php">Créer un compte</a>
+			</div>
 		</div>
 	</div>
-</div> 
 
-<?php
-include("../headerfooter/footer.php");
-?>    
+	<?php
+	include("../headerfooter/footer.php");
+	?>
 </body>
+
 </html>
 
 <?php
--- public_html/user/creeUser.php
+++ public_html/user/creeUser.php
@@ -4,120 +4,122 @@ include("../DBconnect/db_connect.php");
 
 session_start();
 
-if(isset($_SESSION["user"])){
+if (isset($_SESSION["user"])) {
 	header("Location: user.php");
 }
 
 $message_creation = "";
-if(isset($_POST["user_name"]) && isset($_POST["user_pwd"]) && isset($_POST["user_pwd_confirmation"])){
+if (isset($_POST["user_name"]) && isset($_POST["user_pwd"]) && isset($_POST["user_pwd_confirmation"])) {
 	$name = $_POST["user_name"];
 	$pwd = $_POST["user_pwd"];
 	$pwd_confirmation = $_POST["user_pwd_confirmation"];
 
-	if($name == "" || $pwd == "" || $pwd_confirmation == ""){
+	if ($name == "" || $pwd == "" || $pwd_confirmation == "") {
 		$message_creation .= "<p class='error_message'>Erreur : Veuillez remplir tous les champs</p>";
-	}else if(name_existe_user($conn, $name)){
+	} else if (name_existe_user($conn, $name)) {
 		$message_creation .= "<p class='error_message'>Erreur : Identifiant déjà utilisé</p>";
-	}else{
+	} else {
 		include("../lib/fct_user.php");
 
-		if(areLoginsValid($name, $pwd, $pwd_confirmation)){
+		if (areLoginsValid($name, $pwd, $pwd_confirmation)) {
 			insert_user($conn, $name, $pwd);
 			$message_creation .= "<p class='success_message'>Compte créée avec succès !</p>";
-		}else{
+		} else {
 			$message_creation .= "<p class='error_message'>Erreur : L'identifiant et/ou le mot de passe n'est pas valide</p>";
 		}
 
-    }
+	}
 }
 
 ?>
 
 <html>
+
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="../css/default.css">
 	<link rel="stylesheet" href="../css/form.css">
 	<title>Créer un compte</title>
 	<script>
-		function passwordSeeHide(checkboxId, pwdId){
+		function passwordSeeHide(checkboxId, pwdId) {
 			var checkbox = document.getElementById(checkboxId);
 			var pwd = document.getElementById(pwdId);
-			
-			if(checkbox.checked){
+
+			if (checkbox.checked) {
 				pwd.type = "text";
-			}else{
+			} else {
 				pwd.type = "password";
 			}
 		}
 	</script>
-</head>	
-	
+</head>
+
 <body>
-<?php
+	<?php
 
-include("../headerfooter/header.php");?>
-	
-<div id="container">
-	<div id="form_container">
-		<h2 class="form_title">Créer un compte</h2>
+	include("../headerfooter/header.php"); ?>
 
-		<?php
-		echo $message_creation;
-		?>
-	
-		<div id="formulaire">
-			<form method="POST" action="creeUser.php">
-				<div class="form_section">
-					<p class="form_txt">Nom d'utilisateur</p>
-					<input type="text" name="user_name">
-					<p class="form_contrainte">2 à 16 caractères</p>
-				</div>
+	<div id="container">
+		<div id="form_container">
+			<h2 class="form_title">Créer un compte</h2>
 
-				<div class="form_section">
-					<div class="form_header">
-						<p class="form_txt">Mot de passe</p>
+			<?php
+			echo $message_creation;
+			?>
 
-						<div class="pwd_container">
-							<input type="checkbox" id="checkbox_pwd" onclick="passwordSeeHide('checkbox_pwd', 'user_pwd')">
-							<label for="checkbox_pwd">Afficher</label>
-						</div>
+			<div id="formulaire">
+				<form method="POST" action="creeUser.php">
+					<div class="form_section">
+						<p class="form_txt">Nom d'utilisateur</p>
+						<input type="text" name="user_name">
+						<p class="form_contrainte">2 à 16 caractères</p>
 					</div>
 
-					<input type="password" name="user_pwd" id="user_pwd">
-					<p class="form_contrainte">8 caractères ou plus dont au moins : 1 majuscule, 1 minuscule et 1 chiffre</p>
-				</div>
+					<div class="form_section">
+						<div class="form_header">
+							<p class="form_txt">Mot de passe</p>
 
-				<div class="form_section">
-					<div class="form_header">
-						<p class="form_txt">Confirmez le mot de passe</p>
-
-						<div class="pwd_container">
-							<input type="checkbox" id="checkbox_pwd_confirmation" onclick="passwordSeeHide('checkbox_pwd_confirmation', 'user_pwd_confirmation')">
-							<label for="checkbox_pwd_confirmation">Afficher</label>
+							<div class="pwd_container">
+								<input type="checkbox" id="checkbox_pwd"
+									onclick="passwordSeeHide('checkbox_pwd', 'user_pwd')">
+								<label for="checkbox_pwd">Afficher</label>
+							</div>
 						</div>
+
+						<input type="password" name="user_pwd" id="user_pwd">
+						<p class="form_contrainte">8 caractères ou plus dont au moins : 1 majuscule, 1 minuscule et 1
+							chiffre</p>
 					</div>
-					
-					<input type="password" name="user_pwd_confirmation" id="user_pwd_confirmation">
-				</div>
 
-				<input type="submit" value="Créer un compte">
-			</form>
+					<div class="form_section">
+						<div class="form_header">
+							<p class="form_txt">Confirmez le mot de passe</p>
 
-			<a href="connUser.php">Déjà enregistré ? Connecte toi ici</a>
+							<div class="pwd_container">
+								<input type="checkbox" id="checkbox_pwd_confirmation"
+									onclick="passwordSeeHide('checkbox_pwd_confirmation', 'user_pwd_confirmation')">
+								<label for="checkbox_pwd_confirmation">Afficher</label>
+							</div>
+						</div>
+
+						<input type="password" name="user_pwd_confirmation" id="user_pwd_confirmation">
+					</div>
+
+					<input type="submit" value="Créer un compte">
+				</form>
+
+				<a href="connUser.php">Déjà enregistré ? Connecte toi ici</a>
+			</div>
 		</div>
 	</div>
-</div>
 
-<?php
-include("../headerfooter/footer.php");
-?>
+	<?php
+	include("../headerfooter/footer.php");
+	?>
 
 </body>
+
 </html>
 <?php
 include("../DBconnect/db_disconnect.php");
 ?>
-
-
-
