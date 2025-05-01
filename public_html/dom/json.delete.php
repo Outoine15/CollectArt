70fd0c8a2085df10dc7f163dc4c2205e5027d94e
outoine15 <outoine15@gmail.com>
2025-05-01 15:27:37 +0200

    json delete reparé

--- public_html/dom/json.delete.php
+++ public_html/dom/json.delete.php
@@ -1,14 +1,38 @@
 <?php
+session_start();
+
+include("../DBconnect/db_connect.php");
+include("../crud/toile.crud.php");
+
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-include("../DBconnect/db_connect.php");
-$json_file_name="../toilesJSON/";
-$json_file_name.=$_GET["id"];
-$json_file_name.=".json";
-unlink($json_file_name);
-include("../crud/toile.crud.php");
-delete_toile($conn,$_GET["id"]);
-include("../DBconnect/db_disconnect.php");
-header("Location: ../admin/index.php");
+if((isset($_SESSION["user"]) || isset($_SESSION["admin_id"])) && isset($_GET["id"]) && isset($_GET["action"])){
+    $id=$_GET["id"];
+    if(isset($_SESSION["admin_id"])){
+        delete_toile_total($id,$conn);
+    }
+    $toile=select_toile($conn,$id);
+    $id_creator=$toile["id_creator"];
+    if($_SESSION["user"]==$id_creator){
+        delete_toile_total($id,$conn);
+    }
+}
+
+function delete_toile_total($id,$conn){
+    $json_file_name="../toilesJSON/";
+    $json_file_name.=$id;
+    $json_file_name.=".json";
+    unlink($json_file_name);
+    delete_toile($conn,$id);
+
+    include("../DBconnect/db_disconnect.php");
+
+    if($_GET["action"]=="from_user"){
+        header("Location: ../pages/mes_toiles.php");    
+    } else if($_GET["action"]=="from_admin"){
+        header("Location: ../admin/index.php");
+    }
+}
+
 ?>
