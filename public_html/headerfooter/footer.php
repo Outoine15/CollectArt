081a496a4b24a55be9a500177eddde3fc958bedb
outoine15 <outoine15@gmail.com>
2025-05-01 17:48:42 +0200

    update header

--- public_html/headerfooter/footer.php
+++ public_html/headerfooter/footer.php
@@ -3,8 +3,8 @@ echo "
 <footer>
       <div></div>
 	  <div id=\"div_droits\">  
-	  <p>@ Tout droit r&#233;serv&#233; &#224; Bob_et_Alice</p>
-	  <p>Les cookies c'est pas mauvais</p>
+	  <p>@ Tout droit r&#233;serv&#233; &#224; Bob_et_Alice ainsi qu'au groupe 5</p>
+	  <p>Made by  Ludovic Perrin, Christer Moise, Titouan Gastine, Nathan Martin</p>
 	  </div>
 
       <a href=\"https://www.univ-smb.fr/\" id=\"div-image-univsmb\"><image id=\"image-univsmb\" src=\"../images/Logo_USMB_web_vertical_grand_RVB.png\"></image></a>
